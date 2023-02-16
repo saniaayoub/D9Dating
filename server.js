a905d76750b9160cfcea4e72df02a46a61f46628
Nehagoindani <goindanineha64@gmail.com>
2023-02-16 15:46:37 +0500

    server chat

--- server.js
+++ server.js
@@ -1,16 +1,30 @@
-const io = require('socket.io')();
+// const path = require('path');
+const express = require("express");
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const socketio = require('socket.io');
 
+const io = socketio(server);
+app.get("/", function(req, res) {
+  res.write(`<h1>Hello socket</h1> ${PORT}`)
+  res.end
+});
 io.on('connection', (client) => {
-  console.log('a user connected');
-
-  client.on('send message', (data) => {
-    console.log(data);
-    io.emit('receive message', data);
-  });
+  console.log(`⚡: ${client.id} user just connected!`);
+  
+  // client.on('send message', (data) => {
+  //   console.log(data);
+  //   io.emit('receive message', data);
+  //   // client.broadcast.to(message.recieverId).emit( 'message',`${obj}`);
+  // });
 
   client.on('disconnect', () => {
     console.log('user disconnected');
   });
+
 });
 
-io.listen(3000);
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- src/screens/App/Message/Chat/index.js
+++ src/screens/App/Message/Chat/index.js
@@ -7,15 +7,18 @@ import {
   Image,
   ScrollView,
 } from 'react-native';
-import React, {useEffect, useState} from 'react';
-import {useDispatch, useSelector} from 'react-redux';
-import {moderateScale} from 'react-native-size-matters';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { moderateScale } from 'react-native-size-matters';
 import s from './style';
 import Entypo from 'react-native-vector-icons/Entypo';
 import Inicon from 'react-native-vector-icons/Ionicons';
 import Icon from 'react-native-vector-icons/FontAwesome5';
-import {Input, FormControl, Button} from 'native-base';
+import { Input, FormControl, Button } from 'native-base';
 import io from 'socket.io-client';
+import AsyncStorage from '@react-native-async-storage/async-storage'
+//  const socket = io('http://192.168.18.226');
+//     socket.connect();
 
 const messages = [
   {
@@ -72,15 +75,74 @@ const elem = {
   userImage: require('../../../../assets/images/png/u6.png'),
 };
 
-const Chat = ({navigation, route}) => {
+const Chat = ({ navigation, route }) => {
+  const [msg, setMsg] = useState([]);
+  const [input, setInput] = useState('');
+  const [socket, setSocket] = useState(
+    // io('https://monily-chat-server.herokuapp.com'),
+    // io('http://192.168.18.109:3000'),
+    io('http://192.168.18.226:3000'),
+  );
+  const [value, setValue] = useState([])
+
+  const getValueFunction = () => {
+    // Function to get the value from AsyncStorage
+    AsyncStorage.getItem('users').then(
+      (value) =>
+        console.log(value, 'users'),
+        setValue(value),
+      // Setting the value in Text
+    );
+  };
+
+  useEffect(() => {
+    getValueFunction()
+  }, [])
+
+
   const dispatch = useDispatch();
   console.log(route.params);
   const data = route?.params;
-  const [text, setText] = useState(null);
+  const [text, setText] = useState([]);
   const theme = useSelector(state => state.reducer.theme);
   const color = theme === 'dark' ? '#222222' : '#fff';
   const textColor = theme === 'light' ? '#000' : '#fff';
-  const uid = 1;
+  const uid = route.params.id;
+  const name = data.from
+  console.log(uid, 'id');
+
+
+  useEffect(() => {
+    socket.on('receive message', (message) => {
+      console.log(message,'recieve')
+      setText((prevMessages) => [...prevMessages, message]);
+    });
+  }, []);
+
+  const sendMessage = () => {
+    if (input.trim() === '') return;
+    socket.emit('send message', JSON.stringify({
+      text: input,
+      to: uid,
+      from: {
+        id: socket.id,
+        name: name,
+      },
+      avatar: 'https://placeimg.com/140/140/people',
+      time: new Date()
+    }));
+    setMsg((prevMessages) => [...prevMessages, {
+      text: input,
+      to: uid,
+      from: {
+        id: socket.id,
+        name: name,
+      },
+      avatar: 'https://placeimg.com/140/140/people',
+      time: new Date()
+    }]);
+    setInput('');
+  };
 
   const RenderItem = () => {
     return (
@@ -90,17 +152,17 @@ const Chat = ({navigation, route}) => {
         </View>
         <TouchableOpacity
           onPress={() => navigation.navigate('Chat')}
-          style={[s.col, {flex: 0.6, justifyContent: 'flex-end'}]}
+          style={[s.col, { flex: 0.6, justifyContent: 'flex-end' }]}
         >
           <View>
-            <Text style={[s.name, s.nameBold, {color: textColor}]}>
+            <Text style={[s.name, s.nameBold, { color: textColor }]}>
               {data?.from}
             </Text>
           </View>
           <Text style={s.textSmall}>{elem?.item?.text}</Text>
         </TouchableOpacity>
         <View style={s.time}>
-          <Text style={[s.textRegular, {color: textColor}]}>
+          <Text style={[s.textRegular, { color: textColor }]}>
             {elem?.item?.time}
           </Text>
         </View>
@@ -108,11 +170,11 @@ const Chat = ({navigation, route}) => {
     );
   };
   return (
-    <SafeAreaView style={{display: 'flex', flex: 1, backgroundColor: color}}>
-      <View style={[s.container, {backgroundColor: color}]}>
+    <SafeAreaView style={{ display: 'flex', flex: 1, backgroundColor: color }}>
+      <View style={[s.container, { backgroundColor: color }]}>
         <View style={s.header}>
           <TouchableOpacity
-            style={{flex: 0.1}}
+            style={{ flex: 0.1 }}
             onPress={() => navigation.goBack()}
           >
             <Inicon
@@ -135,7 +197,7 @@ const Chat = ({navigation, route}) => {
               />
             </TouchableOpacity>
             <TouchableOpacity onPress={() => navigation.navigate('ViewUser')}>
-              <Text style={[s.name, {color: textColor}]}>
+              <Text style={[s.name, { color: textColor }]}>
                 {messages[0].from}
               </Text>
 
@@ -151,9 +213,9 @@ const Chat = ({navigation, route}) => {
           </TouchableOpacity>
         </View>
         <ScrollView contentContainerStyle={[s.chatContainer]}>
-          {messages.map((elem, i) => {
+          {msg.map((elem, i) => {
             console.log(elem);
-            if (elem?.uid === uid) {
+            if (elem?.to === uid) {
               return (
                 <View style={s.messege} key={i}>
                   <View
@@ -167,13 +229,12 @@ const Chat = ({navigation, route}) => {
                     ]}
                   >
                     <Text style={s.userName}>
-                      {elem?.from}
                       <Text style={s.textSmall1}>{elem?.text}</Text>
                     </Text>
                   </View>
                   <View style={[s.dp, {flex: 0.2}]}>
                     <Image
-                      source={elem?.userImage}
+                      source={{uri: elem?.avatar}}
                       style={s.dp1}
                       resizeMode={'cover'}
                     />
@@ -185,15 +246,14 @@ const Chat = ({navigation, route}) => {
                 <View style={s.messege} key={i}>
                   <View style={[s.dp, {flex: 0.2}]}>
                     <Image
-                      source={elem?.userImage}
+                      source={{uri: "https://placeimg.com/140/140/people","time":"2023-02-16T08:42:26.792Z" }}
                       style={s.dp1}
                       resizeMode={'cover'}
                     />
                   </View>
                   <View style={[s.text, {flex: 0.8}]}>
                     <Text style={s.userName}>
-                      {elem?.from}
-                      <Text style={s.textSmall1}>{elem?.text}</Text>
+                      <Text style={s.textSmall1}>{text}</Text>
                     </Text>
                   </View>
                 </View>
@@ -201,6 +261,45 @@ const Chat = ({navigation, route}) => {
             }
           })}
         </ScrollView>
+        {/* <ScrollView contentContainerStyle={[s.chatContainer]}>
+          {msg.map((message, i) => (
+             <View style={s.messege} key={i}>
+              <View
+                style={[
+                  s.text,
+                  {
+                    flex: 0.8,
+                    backgroundColor: '#333232',
+                    marginRight: moderateScale(10, 0.1),
+                  },
+                ]}
+              >
+                <Text style={s.userName}>
+                <Text style={s.textSmall1}>{message.text}</Text>
+                </Text>
+              
+              </View>
+              <View
+                style={[
+                  s.text,
+                  {
+                    flex: 0.8,
+                    backgroundColor: '#333232',
+                    marginRight: moderateScale(10, 0.1),
+                  },
+                ]}
+              >
+                <Text style={s.userName}>
+                <Text style={s.textSmall1}>{text}</Text>
+                </Text>
+              
+              </View>
+
+
+            </View>
+           
+          ))}
+        </ScrollView> */}
       </View>
       <View style={s.row}>
         <View style={s.input}>
@@ -219,7 +318,7 @@ const Chat = ({navigation, route}) => {
               placeholderTextColor={'#fff'}
               color={'#fff'}
               placeholder="Type Message"
-              onChangeText={v => setText(v)}
+              onChangeText={(text) => setInput(text)}
               size="md"
             />
           </View>
@@ -233,7 +332,8 @@ const Chat = ({navigation, route}) => {
           </TouchableOpacity>
         </View>
         <View style={s.sendBtn}>
-          <TouchableOpacity style={s.circle}>
+          <TouchableOpacity onPress={() => sendMessage()}
+            style={s.circle}>
             <Inicon
               name={'md-send'}
               color={'#8F8A8A'}
