3b5f2633ea0ef226193d80783a37c3febbcf4f92
saniaayoub <saniaayoub555@gmail.com>
2023-03-14 16:54:25 +0500

    push notification

--- index.js
+++ index.js
@@ -7,11 +7,20 @@ import App from './App';
 import {name as appName} from './app.json';
 import {Provider} from 'react-redux';
 import store from './src/Redux/Store';
+import PushNotification from 'react-native-push-notification';
 
 const ReduxApp = () => (
   <Provider store={store}>
     <App />
   </Provider>
 );
+
+// PushNotification.configure({
+//   // (required) Called when a remote is received or opened, or local notification is opened
+//   onNotification: function (notification) {
+//     console.log('NOTIFICATION:', notification);
+//   },
+//   requestPermissions: true,
+// });
 
 AppRegistry.registerComponent(appName, () => ReduxApp);
