e6c03daa21ec068cffc9a9e0b9a98ee37e758354
saniaayoub <saniaayoub555@gmail.com>
2023-03-29 09:40:36 +0500

    funinteraction

--- src/screens/App/Home/FunInteraction/index.js
+++ src/screens/App/Home/FunInteraction/index.js
@@ -52,6 +52,7 @@ const FunInteraction = ({navigation}) => {
   useEffect(() => {
     getID();
     getPosts();
+    getAllUsers();
   }, [isFocused]);
 
   const getID = async () => {
@@ -478,7 +479,7 @@ const FunInteraction = ({navigation}) => {
                 post: elem?.item,
                 screen: 'search',
               });
-              setSearching(false);
+              clear();
             }}
           >
             <Text style={[s.name, s.nameBold, {color: textColor}]}>
@@ -493,6 +494,7 @@ const FunInteraction = ({navigation}) => {
     );
   };
   const onSearch = text => {
+    setSearchText(text);
     if (text) {
       setSearching(true);
       const temp = text.toLowerCase();
@@ -504,8 +506,15 @@ const FunInteraction = ({navigation}) => {
 
       setFiltered(tempList);
     } else {
+      setSearchText('');
       setSearching(false);
     }
+  };
+
+  const clear = () => {
+    setSearchText('');
+    setFiltered([]);
+    setSearching(false);
   };
   return (
     <SafeAreaView style={{display: 'flex', flex: 1, backgroundColor: color}}>
@@ -517,6 +526,7 @@ const FunInteraction = ({navigation}) => {
             placeholder="Search Here"
             placeholderTextColor={'#B9B9B9'}
             onChangeText={onSearch}
+            value={searchText}
             marginTop={moderateScale(10, 0.1)}
             w={'95%'}
             h={moderateScale(37, 0.1)}
@@ -532,7 +542,7 @@ const FunInteraction = ({navigation}) => {
             }
             InputRightElement={
               <TouchableOpacity
-                // onPress={() => handleCancel()}
+                onPress={() => clear()}
                 style={{paddingRight: 10}}
               >
                 {searchText ? (
